374100709d5633d7842254e5be8da960e00e0565
Heng Wen Yang <wenyang.heng@outlook.com>
2023-04-03 22:00:59 +0800

    added chart for current date

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,20 +1,54 @@
-const ctx = document.getElementById('myChart');
+currentDate = getDate();
 
-const myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+// get the data for a specific day
+chrome.storage.local.get(currentDate, function(result) {
+  // check if data exists for this day
+  if (result.hasOwnProperty(currentDate)) {
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    let stats = result[currentDate];
+    const domain = [];
+    const timeSpent = [];
+
+    for(let i = 0; i < stats.length; i++){
+      domain.push(stats[i].domain);
+      timeSpent.push(stats[i].totalTime);
     }
+
+    console.log(domain);
+    console.log(timeSpent);
+
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: domain,
+        datasets:[{
+          label: 'Time spent in milliseconds',
+          data: timeSpent,
+          backgroundColor: 'rgba(251, 41, 41, 0.8)',
+          borderColor: 'rgba(251, 41, 41, 0.8)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            beginAtZero: false
+          },
+        }
+      }
+    });
+  } else {
+    // display a message if no data exists for this day
+    console.log("No data available for this day.");
   }
 });
+
+
+function getDate() {
+  let now = new Date();
+  now.setDate(now.getDate());
+  let date = now.toLocaleDateString('en-GB');
+  return date.split('/').join('-');
+}
