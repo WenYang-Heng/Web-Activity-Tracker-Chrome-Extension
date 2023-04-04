dd974d28958156a3ac787df3629e2123a4560d26
Heng Wen Yang <wenyang.heng@outlook.com>
2023-04-04 22:59:20 +0800

    added chart for weekly data

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,31 @@
 let keys = [];
 let index;
+const ctx = document.getElementById('myChart').getContext('2d');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const weeklyBtn = document.getElementById('weekly');
+const option = {
+  responsive: true,
+  maintainAspectRatio: false,
+  indexAxis: 'y',
+  scales: {
+    x: {
+      beginAtZero: false
+    },
+  }
+}
 
 chrome.storage.local.get(null, function(result){
   keys = Object.keys(result);
   index = keys.length - 1;
-  updateChart();
+  dailyChart();
 });
 
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
-
-// get the data for a specific day
-function updateChart(){
+function dailyChart(){
   let currentDate = keys[index];
   chrome.storage.local.get(currentDate, function(result) {
     // check if data exists for this day
     if (result.hasOwnProperty(currentDate)) {
-      const ctx = document.getElementById('myChart').getContext('2d');
       let canvas  = Chart.getChart("myChart");
 
       //Charts have to be destroyed before creating another one
@@ -46,16 +55,7 @@ function updateChart(){
             barThickness: 45
           }]
         },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          indexAxis: 'y',
-          scales: {
-            x: {
-              beginAtZero: false
-            },
-          }
-        }
+        options: option
       });
     } else {
       // display a message if no data exists for this day
@@ -64,12 +64,54 @@ function updateChart(){
   });  
 }
 
+function weeklyChart(){
+  chrome.storage.local.get(null, function(result){
+    console.log(result);
+    const date = [];
+    const totalTime = [];
+    for(let dateKey in result){
+      let data = result[dateKey];
+      console.log(dateKey);
+      date.push(dateKey);
+      let dailySum = 0;
+      for(let i = 0; i < data.length; i++){
+        console.log(data[i].domain, data[i].totalTime);
+        dailySum += data[i].totalTime;
+      }
+      console.log(dailySum);
+      totalTime.push(dailySum);
+    }
+
+    let canvas  = Chart.getChart("myChart");
+
+    if(canvas != undefined){
+      canvas.destroy();
+    }
+
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data:{
+        labels: date,
+        datasets: [{
+          label: 'Total time spent for each day',
+          data: totalTime,
+          backgroundColor: 'rgba(251, 41, 41, 0.8)',
+          borderColor: 'rgba(251, 41, 41, 0.8)',
+          borderWidth: 0,
+          barThickness: 45
+        }],
+      },
+      options: option
+    });
+  });
+}
+
 leftBtn.addEventListener('click', function(){
   index--;
   if(index < 0){
     index = keys.length - 1;
   }
-  updateChart();
+  dailyChart();
 });
 
 rightBtn.addEventListener('click', function(){
@@ -77,5 +119,9 @@ rightBtn.addEventListener('click', function(){
   if(index > keys.length - 1){
     index = 0;
   }
-  updateChart();
+  dailyChart();
+});
+
+weeklyBtn.addEventListener('click', function(){
+  weeklyChart();
 });
