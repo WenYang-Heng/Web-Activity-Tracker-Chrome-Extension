7c7e975d8f5873d41fd5ad3f9d406e909a8a076b
Heng Wen Yang <wenyang.heng@outlook.com>
2023-04-17 15:09:56 +0800

    onActivated and onUpdated revised

--- background.js
+++ background.js
@@ -6,13 +6,22 @@ let currentDate;
 let idleTime = 0;
 let totalIdleTime = 0;
 const idleDetection = 15;
+const extensionInternal = "chrome://extensions";
+const newTab = "newtab";
 
 // New function to initialize the current domain when Chrome starts
 function initializeCurrentDomain() {
   chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-    if (activeTab && activeTab.url && !activeTab.url.startsWith("chrome://")) {
-      currentDomain = new URL(activeTab.url).hostname;
-      startTime = new Date();
+    if (activeTab && activeTab.url && !activeTab.url.startsWith(extensionInternal)) {
+        currentDomain = new URL(activeTab.url).hostname;
+        if(currentDomain === newTab){
+            previousDomain = currentDomain;
+            console.log("current: " + currentDomain);
+            console.log("previous: " + previousDomain);
+        }
+        else{
+            startTime = new Date();     
+        }
     }
   });
 }
@@ -21,73 +30,98 @@ function initializeCurrentDomain() {
 initializeCurrentDomain();
 
 chrome.tabs.onCreated.addListener(function (tab) {
-    if (previousDomain !== null) {
+    if(tab.url.startsWith("chrome://newtab") && previousDomain === newtab){
+        console.log("opened an empty tab");
+        currentDomain = newTab;
+        previousDomain = currentDomain;
+    }
+    else if (previousDomain !== null && previousDomain !== newTab) {
       timeSpent();
       if (timeOnSite > 0) {
-        storage();
-        console.log("Time spent on " + previousDomain + ": " + timeOnSite);
+        storage(previousDomain, timeOnSite);
+        console.log("New tab created. Time spent on " + previousDomain + ": " + timeOnSite);
+        previousDomain = currentDomain;
       }
     }
-  });
+    else{
+        console.log("opened another empty tab");
+    }
+});
 
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-      if (tab.url && tab.status === "complete" && !tab.url.startsWith("chrome://")) {
-        let url = new URL(tab.url);
-        currentDomain = url.hostname;
-        timeSpent(); // Move this line out of the if condition
-        if (currentDomain !== previousDomain) {
-          if (timeOnSite > 0) {
-            storage();
-            console.log("Time spent on " + previousDomain + ": " + timeOnSite);
-          }
-          startTime = new Date();
-        } else {
-          console.log("switched to the same domain");
+chrome.tabs.onActivated.addListener(function (activeInfo){
+    chrome.tabs.get(activeInfo.tabId, function (tab){
+        if (tab.url && tab.status === "complete" && !tab.url.startsWith(extensionInternal)){
+            currentDomain = new URL(tab.url).hostname;
+            //if switch to an empty tab and previous tab is not null
+            if(currentDomain === newTab && previousDomain !== newTab){
+                timeSpent();
+                storage(previousDomain, timeOnSite);
+                startTime = new Date();
+                previousDomain = currentDomain;
+            }
+            else if(previousDomain === newTab && currentDomain !== newTab){
+                console.log("switched from empty tab to " + currentDomain + ". Start tracking now");
+                previousDomain = currentDomain;
+                startTime = new Date();
+            }
+            else if(currentDomain !== previousDomain){
+                timeSpent();
+                storage(previousDomain, timeOnSite);
+                startTime = new Date();
+                previousDomain = currentDomain;
+            }
+            else{
+                previousDomain = currentDomain;
+                console.log("switch to same domain");
+            }
         }
-      }
     });
-  });
+});
   
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    console.log("page refreshed");
-    if(changeInfo.url && tab.url && !tab.url.startsWith('chrome://')){
-        //check if the url has changed
+    if(changeInfo.url && tab.url && !tab.url.startsWith(extensionInternal)){
+        console.log("page has refreshed.");
         currentDomain = new URL(changeInfo.url).hostname;
-        if(changeInfo.url && currentDomain !== previousDomain){
-            timeSpent();
-            if(timeOnSite > 0){
-                storage();
-                console.log("time spent on " + previousDomain + " :" + timeOnSite);         
-                startTime = new Date();       
+        if(currentDomain === newTab){
+            previousDomain = currentDomain;
+        }
+        else if(previousDomain === newTab && currentDomain !== newTab){
+            startTime = new Date();
+            console.log("refreshed from " + previousDomain + " to " + currentDomain + ". Start tracking now");
+            previousDomain = currentDomain;
+        }
+        else if(previousDomain !== newTab && currentDomain !== newTab){
+            if(previousDomain !== currentDomain){
+                timeSpent();
+                storage(previousDomain, timeOnSite);
+                startTime = new Date();
+                previousDomain = currentDomain;
             }
-        }else{
-            console.log("page refreshed to same domain: " + currentDomain);
         }
     }
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    // Check if there are any other tabs open in the window
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-        if (tabs.length === 0) {
-            // previousDomain = new URL(tabs.url).hostname;
-            timeSpent();
-            // The closed tab was the last one in the window
-            // Track the time spent on the previous domain
-            console.log("time spent on " + previousDomain + " :" + timeOnSite); 
-            storage();
-            // Reset the previous domain to null
-        } 
-    });
-});
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+//     // Check if there are any other tabs open in the window
+//     chrome.tabs.query({currentWindow: true}, function(tabs) {
+//         if (tabs.length === 0) {
+//             // previousDomain = new URL(tabs.url).hostname;
+//             timeSpent();
+//             // The closed tab was the last one in the window
+//             // Track the time spent on the previous domain
+//             console.log("time spent on " + previousDomain + " :" + timeOnSite); 
+//             storage();
+//             // Reset the previous domain to null
+//         } 
+//     });
+// });
 
 function timeSpent(){
     console.log("Calculataing time, idle time is :" + totalIdleTime)
     timeOnSite = (new Date() - startTime) - totalIdleTime;
 }
 
-function storage(){
+function storage(domainToBeInserted, timeOnDomain){
     currentDate = getDate();
 
     chrome.storage.local.get(null, function(result){
@@ -99,15 +133,15 @@ function storage(){
             return dateA - dateB;
           });
 
-        if(keys.length > 7){
-            console.log("keys exceeded");
-            let oldestKey = keys.shift();
-            // chrome.storage.local.remove(oldestKey, function(){
-            //     console.log(`Deleted oldest key: ${oldestKey}`);
-            // });
-        }
+        // if(keys.length > 7){
+        //     console.log("keys exceeded");
+        //     let oldestKey = keys.shift();
+        //     // chrome.storage.local.remove(oldestKey, function(){
+        //     //     console.log(`Deleted oldest key: ${oldestKey}`);
+        //     // });
+        // }
 
-        console.log("Number of keys in storage: " + keys.length);
+        // console.log("Number of keys in storage: " + keys.length);
 
         // chrome.storage.local.remove(currentDate, function(){
         //     console.log("all keys cleared");
@@ -124,10 +158,10 @@ function storage(){
             });
         }
 
-        if(previousDomain !== null){
+        if(domainToBeInserted !== null && timeOnDomain !== null){
             let record = {
-                domain: previousDomain,
-                totalTime: timeOnSite
+                domain: domainToBeInserted,
+                totalTime: timeOnDomain
             }
 
             //check if domain exists in the object
@@ -136,8 +170,8 @@ function storage(){
             //check if result has an object
             if(stats){
                 for(let i = 0; i < stats.length; i++){
-                    if(stats[i].domain === previousDomain){
-                        stats[i].totalTime += timeOnSite;
+                    if(stats[i].domain === domainToBeInserted){
+                        stats[i].totalTime += timeOnDomain;
                         check = true;
                         break;
                     }
@@ -155,12 +189,10 @@ function storage(){
             }
 
             chrome.storage.local.set({[currentDate]: stats}, function(){
-                console.log(`Pushed data to ${currentDate}`);
-                console.log(result[currentDate]);
+                // console.log(`Pushed ${record.domain, record.totalTime}  to ${currentDate}`);
+                console.log("Pushed " + record.domain + ", " + record.totalTime + " to " + currentDate);
             });       
         }
-
-        previousDomain = currentDomain;
         totalIdleTime = 0;
     });
 }
